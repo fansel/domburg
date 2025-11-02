58f2544cb08169e240344e655f542c5aebcc2528
Felix Mansel <56174409+fansel@users.noreply.github.com>
2025-11-02 22:11:14 +0100

    v1.9

--- prisma/migrations/20251101175500_add_season_booking_rules/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "PricingPhase" ADD COLUMN     "minNights" INTEGER,
-ADD COLUMN     "saturdayToSaturday" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "warningMessage" TEXT;
-
--- prisma/migrations/20251102013436_add_adults_and_children_to_booking/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable: Add numberOfAdults and numberOfChildren, remove numberOfGuests
-ALTER TABLE "Booking" ADD COLUMN "numberOfAdults" INTEGER NOT NULL DEFAULT 1;
-ALTER TABLE "Booking" ADD COLUMN "numberOfChildren" INTEGER NOT NULL DEFAULT 0;
-
--- Migrate existing data: assume all numberOfGuests are adults
-UPDATE "Booking" SET "numberOfAdults" = "numberOfGuests", "numberOfChildren" = 0;
-
--- Remove old column
-ALTER TABLE "Booking" DROP COLUMN "numberOfGuests";
